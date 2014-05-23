d6ea404d9c094efda27fff09a5f85a70c9c83680
Austin Buchan <austin.d.buchan@gmail.com>
2014-05-22 17:13:02 -0700

    Adding imaging files

---
+++ Makefile
@@ -0,0 +1,10 @@
+CHISEL_SIFT=/home/abuchan/projects/chisel-sift
+
+netlist: system/system.xmp
+
+image: image_sdcard.sh xillinux.img
+	./image_sdcard.sh xillinux.img
+
+program:
+
+
---
+++ image_sdcard.sh
@@ -0,0 +1,19 @@
+read -p "Enter device to image: " sdcard
+
+read -p "WARNING! You are about to image $sdcard. Do you wish to continue (yes/no)?: " yn
+
+while true; do
+  case $yn in
+    yes ) echo "Imaging $sdcard with $1:";
+      # Use pv (pipe viewer) to display progress if it exists)
+      if hash pv 2>/dev/null; then
+        pv $1 | dd bs=4M of=$sdcard
+      else
+        dd if=$1 bs=4M of=$sdcard
+      fi
+      echo "Imaging $sdcard complete.";
+      break;;
+    * ) echo "Imaging $sdcard aborted"; break;;
+  esac
+done
+
